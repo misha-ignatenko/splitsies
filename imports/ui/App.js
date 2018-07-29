51025a0f3ff5f1dca7580ada00748d5c7ddae484
Mykhaylo Ignatenko <mykhaylo.ignatenko@gmail.com>
2018-07-28 21:33:26 -0700

    logo done plus begun reformatting

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 
+import { Row, Col } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { Tasks } from '../api/tasks.js';
 
-import Task from './Task.js';
+import Product from './Product.js';
 import AccountsUIWrapper from './AccountsUIWrapper.js';
 
 // App component - represents the whole app
@@ -13,80 +15,33 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            hideCompleted: false,
-        };
+        this.state = {};
     }
 
-    handleSubmit(event) {
-        event.preventDefault();
-
-        // Find the text field via the React ref
-        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
-
-        Meteor.call('tasks.insert', text);
-
-        // Clear form
-        ReactDOM.findDOMNode(this.refs.textInput).value = '';
-    }
-
-    toggleHideCompleted() {
-        this.setState({
-            hideCompleted: !this.state.hideCompleted,
-        });
-    }
-
-    renderTasks() {
-        let filteredTasks = this.props.tasks;
-        if (this.state.hideCompleted) {
-            filteredTasks = filteredTasks.filter(task => !task.checked);
-        }
-        return filteredTasks.map((task) => {
-            const currentUserId = this.props.currentUser && this.props.currentUser._id;
-            const showPrivateButton = task.owner === currentUserId;
+    renderProducts() {
+        return this.props.products.map((product) => {
 
             return (
-                <Task
-                    key={task._id}
-                    task={task}
-                    showPrivateButton={showPrivateButton}
+                <Product
+                    key={product._id}
                 />
             );
-        });
+        })
     }
 
     render() {
         return (
             <div className="container">
                 <header>
-                    <h1>Todo List ({this.props.incompleteCount})</h1>
-
-                    <label className="hide-completed">
-                        <input
-                            type="checkbox"
-                            readOnly
-                            checked={this.state.hideCompleted}
-                            onClick={this.toggleHideCompleted.bind(this)}
-                        />
-                        Hide Completed Tasks
-                    </label>
-
-                    <AccountsUIWrapper />
-
-                    { this.props.currentUser ?
-                        <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-                            <input
-                                type="text"
-                                ref="textInput"
-                                placeholder="Type to add new tasks"
-                            />
-                        </form> : ''
-                    }
+                    <Row>
+                        <Col sm="8"><img src='images/logo.png' style={{height: 75}}/></Col>
+                        <Col sm="4"><AccountsUIWrapper /></Col>
+                    </Row>
                 </header>
 
-                <ul>
-                    {this.renderTasks()}
-                </ul>
+                <Row>
+                    {this.renderProducts()}
+                </Row>
             </div>
         );
     }
@@ -97,7 +52,7 @@ export default withTracker(() => {
 
     return {
         tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
-        incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
         currentUser: Meteor.user(),
+        products: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
     };
 })(App);
---
+++ imports/ui/Product.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Col } from 'reactstrap';
+
+// Task component - represents a single todo item
+export default class Product extends Component {
+
+    render() {
+
+        return (
+            <Col sm="4">
+                <Card>
+                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                        <Button>Button</Button>
+                    </CardBody>
+                </Card>
+            </Col>
+        );
+    }
+}
